fa5a0407a45e0c7917e1a88ab8f138b349a5f6a1
Felipe Souza <felipesouza0789@gmail.com>
2026-07-12 23:59:23 -0300

    inserção da navbar

--- Site/index.php
+++ Site/index.php
@@ -4,7 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TecNutri</title>
+    <link rel="stylesheet" href="navbar.css">
 </head>
+
 <body>
+    <header>
+        <nav class="navbar">
+
+            <div class="logo">
+                <p>TecNutri</p>
+            </div>
+
+            <ul class="menu">
+                <li><a href="#">Cadastra-se</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
 </body>
 </html>
