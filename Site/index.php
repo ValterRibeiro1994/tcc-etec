caabb322b268436628d892453f9405581ddab2f8
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-07-18 15:23:46 -0300

    uni as branch já que é ponto inicial

--- Site/index.php
+++ Site/index.php
@@ -4,7 +4,77 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TecNutri</title>
+    <!--link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
+    <header>
+        <nav class="nav">
+            <div class="logo">
+                <p>TecNutri</p>
+            </div>
+            <ul class="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Dietas</a></li>
+                <li><a href="#">Feed</a></li>
+                <li><a href="#">Sobre</a></li>
+            </ul>
+            <div class="buttons">
+                <button class="entrar">Entrar</button>
+                <button class="cadastro">Cadastra-se</button>
+            </div>
+        </nav>
+    </header>
+
+    <br><br>
+
+    <section class="quem-somos">
+        <div>
+        <small>Nutrição Conciente sempre</small>
+        <h1>Quem Somos</h1>
+        <p>Somos uma organização sem fins lucrativos dedicada a ajudar pessoas de baixa renda, oferecendo orientação nutricional gratuita, apoio na elaboração de dietas personalizadas e auxílio na organização alimentar. Nosso objetivo é promover mais saúde, qualidade de vida e acesso à alimentação adequada para todos.</p>
+        <br><br>
+        <button class="btnCon">Conheça nossos Métodos</button>
+        </div>
+         
+        <div class="prato">
+            <img src="img/Healthy Eating Ideas for a Balanced Lifestyle.jpg" alt="Prato saudável">
+        </div>
+    </section>     
+    
+    <br><br>
+
+    <section class="planos">
+        <ul class="lista">
+            <li>
+            <strong>3k+</strong>
+            <small>PLANOS CRIADOS</small>
+        </li>
+        <li>
+            <strong>7</strong>
+            <small>ESPECIALISTAS</small>
+        </li>
+        <li>
+            <strong>90%</strong>
+            <small>ADERÊNCIA MENSAL</small>
+        </li>
+        <li>
+            <strong>3 ANOS</strong>
+            <small>ESTUDANDO COMIDA EM GERAL</small>
+        </li>
+        </ul>
+    </section>
+
+    <br><br>
+
+    <section>
+
+    </section>
+
+<script>https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js</script>
 </body>
 </html>
