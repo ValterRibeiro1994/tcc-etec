1aae035270604c02cc47c045bb038ef89ac53b45
Felipe Souza <felipesouza0789@gmail.com>
2026-09-07 22:07:08 -0300

    Alterações no front end
    mudanças na pagina de cadastro e login, ajuste na navbar

---
+++ app-web/app/view/scripts/navbar.js
@@ -0,0 +1,7 @@
+const btnCadastro = document.getElementById('btn-cadastro');
+
+if (btnCadastro) {
+    btnCadastro.addEventListener('click', function () {
+        window.location.href = 'cadastro-denunciante.html';
+    });
+}
