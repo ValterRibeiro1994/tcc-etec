c4885366d904b31dbc0ca9c247ab960bc3ff9671
Felipe Souza <felipesouza0789@gmail.com>
2026-09-07 23:43:06 -0300

    pagina de selecionar perfil

--- app-web/app/view/scripts/navbar.js
+++ app-web/app/view/scripts/navbar.js
@@ -1,7 +1,16 @@
 const btnCadastro = document.getElementById('btn-cadastro');
+const btnEntrar = document.getElementById('btn-entrar');
 
+// BOTÃO CADASTRE-SE -> Vai para a escolha de CADASTRO
 if (btnCadastro) {
     btnCadastro.addEventListener('click', function () {
-        window.location.href = 'cadastro-denunciante.html';
+        window.location.href = 'seleciona-cadastro.html';
+    });
+}
+
+// BOTÃO ENTRAR -> Vai para a escolha de LOGIN
+if (btnEntrar) {
+    btnEntrar.addEventListener('click', function () {
+        window.location.href = 'seleciona-login.html';
     });
 }
