2a5250829dc03795a77af4cd4e69cced28cb3b00
VALTER SERGIO RIBEIRO TERTULIANO <valtertert@gmail.com>
2026-09-09 15:23:03 -0300

    Questões desafiadoras sendo planejadas

--- app-web/app/controller/userController.php
+++ app-web/app/controller/userController.php
@@ -2,6 +2,13 @@
 
 class UserController {
     public function index(array $requisicao){
+        /** Verificar se existe um token vindo do cliente
+         * if (metoodo == get && array_key_exists(get[token])):
+         *      if validarToken();
+         *          exibirPaginaHome
+         *     
+         * 
+         */
         return RespostaProcesso::respostaProcesso("app/view/paginas/selecionar-cadastro.html", true, formato: "text/html");
     }
 
