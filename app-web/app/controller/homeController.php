0558280cebac2b1580362669917b0088839750c0
FelipeRk. <feliper210209@gmail.com>
2026-09-20 16:28:25 -0300

    Progresso na tela de perfil
    coisas a fazer:
    melhorar os dropdown que n faço ideia de como vou fazer isso
    reponsividade no span da imagem e na tag, vou deixar para ver isso em sala de aula com o lucas
    
    criar um molde, ja que a lista vai ser gerada através da linguagem pegando o banco de dados mas vou ver isso com o valter
    melhorar os botões, adicionar funcionalidade e tem que corrigir os links ainda

--- app-web/app/controller/homeController.php
+++ app-web/app/controller/homeController.php
@@ -7,11 +7,16 @@ class HomeController {
         }
 
         if ($requisicao['metodo'] == "GET"){
+<<<<<<< Updated upstream
             // A página principal do site deve ser chamada
             $dados = [
                 "pagina" => "app/view/paginas/index.html",
                 "tipo-resposta"=>"text/html"
             ];
+=======
+            return RespostaProcesso::respostaProcesso("app/view/paginas/perfil.html", status: true, formato: "text/html");
+        }
+>>>>>>> Stashed changes
 
             $mensagem = "
             O javascript deve guardar o token caso o usuario ja tenha feito login,
